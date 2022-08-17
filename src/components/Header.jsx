d6633339465842b0163d6a7a531eef5da47e32e4
Aleksey <epsilonalexman@gmail.com>
2022-08-17 12:51:21 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom'
+
+export const Header = () => {
+  return (
+    <div>
+        <NavLink></NavLink>
+    </div>
+  )
+}
+
+export default Header
